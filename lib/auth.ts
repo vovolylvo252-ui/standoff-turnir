57729f0c9e081a13c9a6e5381776b7409967c9b1
Askar <askarikus101@gmail.com>
2026-07-23 02:23:59 +0300

    Fix prisma import path

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "@/lib/prisma";
+import { prisma } from "./prisma";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
